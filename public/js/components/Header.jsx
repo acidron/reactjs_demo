f90f062208ad29c0071fceea0a80e7a21a52a945
DroN <acidron@gmail.com>
2015-07-05 22:30:38 +0300

    Adaptive layout for header

--- public/js/components/Header.jsx
+++ public/js/components/Header.jsx
@@ -7,11 +7,11 @@ var Header = React.createClass({
 		return (
 			<div className="header">
 				<div className="container">
-					<div className="col-sm-1"><a href="#messages"><img src="imgs/ico_lists.png" alt="" /></a></div>
-					<div className="col-sm-4">{this.props.title}</div>
-					<div className="withtext col-sm-5 text-right"><UserName /></div>
-					<div className="col-sm-1"><a href="#profile"><img src="imgs/ico_settings.png" alt="" /></a></div>
-					<div className="col-sm-1"><a href="#signin"><img onClick={this.logout} src="imgs/ico_logout.png" alt="" /></a></div>
+					<div className="col-xs-1"><a href="#messages"><img src="imgs/ico_lists.png" alt="" /></a></div>
+					<div className="col-xs-11 col-sm-4">{this.props.title}</div>
+					<div className="hidden-xs col-sm-5 text-right"><UserName /></div>
+					<div className="hidden-xs col-sm-1"><a href="#profile"><img src="imgs/ico_settings.png" alt="" /></a></div>
+					<div className="hidden-xs col-sm-1"><a href="#signin"><img onClick={this.logout} src="imgs/ico_logout.png" alt="" /></a></div>
 				</div>
 			</div>
 		);
